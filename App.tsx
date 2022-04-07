e26629b20be6912467a52c000c48c0320d655eb8
Jon Samp <sampjon@gmail.com>
2022-04-07 16:58:43 -0400

    update 2

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Update 1</Text>
+      <Text>Update 2</Text>
       <StatusBar style='auto' />
     </View>
   );
